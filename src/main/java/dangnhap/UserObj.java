4c45152a5b366ec670e00fed81716de314a25f8c
VK <levankhoa546248@gmail.com>
2021-10-29 13:38:56 +0700

    commit code dgxcmt
    commit code dgxcmt

---
+++ src/main/java/dangnhap/UserObj.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dangnhap;
+
+/**
+ *
+ * @author Tuan
+ */
+public class UserObj {
+    private String tendangnhap;
+    private String matkhau;
+    private String phongkham;
+    private String matkhaucu;
+
+    public String getMatkhaucu() {
+        return matkhaucu;
+    }
+
+    public void setMatkhaucu(String matkhaucu) {
+        this.matkhaucu = matkhaucu;
+    }
+
+    public String getTendangnhap() {
+        return tendangnhap;
+    }
+
+    public void setTendangnhap(String tendangnhap) {
+        this.tendangnhap = tendangnhap;
+    }
+
+    public String getMatkhau() {
+        return matkhau;
+    }
+
+    public void setMatkhau(String matkhau) {
+        this.matkhau = matkhau;
+    }
+
+    public String getPhongkham() {
+        return phongkham;
+    }
+
+    public void setPhongkham(String phongkham) {
+        this.phongkham = phongkham;
+    }
+    
+}
