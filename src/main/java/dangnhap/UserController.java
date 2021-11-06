460e4da4942329c0786f9387a166eba23655274f
VK <levankhoa546248@gmail.com>
2021-11-06 16:21:35 +0700

    code login

--- src/main/java/dangnhap/UserController.java
+++ src/main/java/dangnhap/UserController.java
@@ -12,10 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-
-/**
- * @author Tuan
- */
 @Controller
 public class UserController {
 
