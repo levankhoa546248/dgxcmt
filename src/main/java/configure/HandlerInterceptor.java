87c524993ef8213e6b3c6be240fbc1b65c9a6d7a
VK <levankhoa546248@gmail.com>
2021-11-15 15:22:00 +0700

    dgxcmt release

--- src/main/java/configure/HandlerInterceptor.java
+++ src/main/java/configure/HandlerInterceptor.java
@@ -1,11 +1,8 @@
 package configure;
 
-import org.springframework.lang.Nullable;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,7 +12,7 @@ public class HandlerInterceptor extends HandlerInterceptorAdapter {
                              HttpServletResponse response,
                              Object handler) throws Exception {
         if (handler instanceof HandlerMethod) {
-//            request.getSession().setMaxInactiveInterval(10);
+            request.getSession().setMaxInactiveInterval(120);
         }
         return true;
     }
